d8831df5afd4d3c5024a48f5e0368d109bb6f0a6
Jason <jasondhakhwa19@gmail.com>
2022-12-14 11:23:44 +0545

    added alt tag

--- components/HomePageComponents/Gallery.jsx
+++ components/HomePageComponents/Gallery.jsx
@@ -18,31 +18,31 @@ const Gallery = () => {
                 <div className="flex flex-wrap -m-1 md:-m-2 flex-col md:flex-row">
                     <div className="flex flex-wrap md:w-1/2 ">
                         <div className="w-1/2 p-1 md:p-2 h-40 overflow-hidden">
-                            <img alt="gallery" className="block object-cover object-center w-full h-full rounded-lg"
-                                src={`${process.env.DOMAIN}${galleries?.[0]?.image}`} />
+                            <img className="block object-cover object-center w-full h-full rounded-lg"
+                                src={`${process.env.DOMAIN}${galleries?.[0]?.image}`} alt={galleries?.[0]?.title} />
                         </div>
                         <div className="w-1/2 p-1 md:p-2 h-40 overflow-hidden">
-                            <img alt="gallery" className="block object-cover object-center w-full h-full rounded-lg"
-                                src={`${process.env.DOMAIN}${galleries?.[1]?.image}`} />
+                            <img className="block object-cover object-center w-full h-full rounded-lg"
+                                src={`${process.env.DOMAIN}${galleries?.[1]?.image}`} alt={galleries?.[1]?.title} />
                         </div>
                         <div className="w-full p-1 md:p-2 flex-grow  h-60">
-                            <img alt="gallery" className="block object-cover object-center w-full h-full rounded-lg"
-                                src={`${process.env.DOMAIN}${galleries?.[2]?.image}`} />
+                            <img className="block object-cover object-center w-full h-full rounded-lg"
+                                src={`${process.env.DOMAIN}${galleries?.[2]?.image}`} alt={galleries?.[2]?.title} />
                         </div>
                     </div>
                     <div className="flex flex-wrap md:w-1/2 flex-col-reverse md:flex-col">
                         <div className="w-full p-1 md:p-2 h-60">
-                            <img alt="gallery" className="block object-cover object-center w-full h-full rounded-lg"
-                                src={`${process.env.DOMAIN}${galleries?.[3]?.image}`} />
+                            <img className="block object-cover object-center w-full h-full rounded-lg"
+                                src={`${process.env.DOMAIN}${galleries?.[3]?.image}`} alt={galleries?.[3]?.title} />
                         </div>
                         <div className="flex h-40">
                             <div className="w-1/2 p-1 md:p-2">
-                                <img alt="gallery" className="block object-cover object-center w-full h-full rounded-lg"
-                                    src={`${process.env.DOMAIN}${galleries?.[4]?.image}`} />
+                                <img className="block object-cover object-center w-full h-full rounded-lg"
+                                    src={`${process.env.DOMAIN}${galleries?.[4]?.image}`} alt={galleries?.[4]?.title} />
                             </div>
                             <div className="w-1/2 p-1 md:p-2">
-                                <img alt="gallery" className="block object-cover object-center w-full h-full rounded-lg"
-                                    src={`${process.env.DOMAIN}${galleries?.[5]?.image}`} />
+                                <img className="block object-cover object-center w-full h-full rounded-lg"
+                                    src={`${process.env.DOMAIN}${galleries?.[5]?.image}`} alt={galleries?.[5]?.title} />
                             </div>
                         </div>
                     </div>
