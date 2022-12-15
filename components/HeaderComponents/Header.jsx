5f725298ee6e3b2c1eb3d62616542f63e3394f52
Jason <jasondhakhwa19@gmail.com>
2022-12-15 14:42:17 +0545

    top scroll fixed

--- components/HeaderComponents/Header.jsx
+++ components/HeaderComponents/Header.jsx
@@ -3,7 +3,7 @@ import HeaderTailwind from "./HeaderTailwind";
 
 const Header = () => {
   return (
-    <div className=' '>
+    <div id='navbar'>
       <div className='relative bg-green-500 top-0 flex flex-row justify-center sm:justify-between items-center text-white  px-8 sm:px-12 md:px-14 lg:px-18 py-2'>
         <div className='text-md font-bold hidden sm:block whitespace-nowrap'>Contact: <span className='font-bold'>9823721737</span></div>
         <div className='flex sm:gap-6 header-socials justify-between sm:justify-end w-full'>
