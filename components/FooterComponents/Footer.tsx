f0293458e66d099231cecba05afe2e8d7fda34a9
Jason <jasondhakhwa19@gmail.com>
2022-12-19 11:46:25 +0545

    padding fixing

--- components/FooterComponents/Footer.tsx
+++ components/FooterComponents/Footer.tsx
@@ -9,7 +9,7 @@ const Footer = () => {
   return (
     <footer className='text-white w-full h-max sm:h-max '>
       <div className='flex h-max w-full flex-wrap lg:flex-row items-start md:items-start justify-between lg:columns-3
-      bg-neutral-800 px-8 sm:px-12 md:px-14 lg:px-18 2xl:px-48 pt-10 md:pt-20 pb-10 overflow-x-hidden gap-5'>
+      bg-neutral-800 px-8 sm:px-12 md:px-14 lg:px-18 2xl:px-48 pt-10 md:py-20 pb-10 overflow-x-hidden gap-5'>
         <CompanyInfo />
         <FooterPageLinks />
         <FooterContactUs />
