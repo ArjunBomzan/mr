51cdcaef449b96353f010f2331d0b6c583bfa4b4
Sagar <dev.sagartmg@gmail.com>
2022-12-22 12:19:19 +0545

    scrolltotop minor fix

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,6 @@ import "../styles/index.css";
 import "../styles/training.css";
 // import 'tw-elements';
 
-import Link from "next/link";
 import { useEffect } from "react";
 import Footer from "../components/FooterComponents/Footer";
 
@@ -18,16 +17,18 @@ function MyApp({ Component, pageProps }) {
   }, []);
 
   return <>
-    
+
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,300" rel="stylesheet" type="text/css"></link>
     <Component {...pageProps} />
     <Footer />
 
-    <Link href="#navbar" id='scroll-to-top' scroll={false}>
+    <span id="scroll-to-top" onClick={() => {
+      window.scroll(0, 0)
+    }}>
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
         <path d="M246.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L224 109.3 361.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160zm160 352l-160-160c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L224 301.3 361.4 438.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3z" />
       </svg>
-    </Link>
+    </span>
   </>
 }
 
