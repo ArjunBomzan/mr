7a349b92f82f31292b948a29d1ffba603d06d129
Sagar <dev.sagartmg@gmail.com>
2023-02-28 10:56:31 +0545

    read-more in courses overview

--- components/HeaderComponents/HeaderTailwind.jsx
+++ components/HeaderComponents/HeaderTailwind.jsx
@@ -13,7 +13,7 @@ const HeaderTailwind = () => {
 
 
     return (
-        <nav className="sticky-top bg-white  w-full flex flex-wrap items-center justify-between py-4 shadow-lg navbar navbar-expand-lg navbar-light  pt-4 pb-4 px-6 sm:px-12 md:px-14 lg:px-18 ">
+        <nav id='navbar--sticky' className="sticky-top bg-white  w-full flex flex-wrap items-center justify-between py-4 shadow-lg navbar navbar-expand-lg navbar-light  pt-4 pb-4 px-6 sm:px-12 md:px-14 lg:px-18 ">
             <div className="container-fluid w-full flex flex-wrap items-center justify-between">
                 <HeaderLeft />
                 <button className=" navbar-toggler text-gray-500 border-0 hover:shadow-none hover:no-underline py-2 px-2.5 -mx-2.5 bg-transparent focus:outline-none focus:ring-0 focus:shadow-none focus:no-underline " type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
--- components/TrainingComponents/TrainingComponents/TrainingOverview.jsx
+++ components/TrainingComponents/TrainingComponents/TrainingOverview.jsx
@@ -7,13 +7,43 @@
     scrollbar-width: none;
 */
 import Image from "next/image"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import DownArrow from "../../../assets/icons/iconmonstr-arrow-down-thin-48-_1_.webp"
 import UpArrow from "../../../assets/icons/iconmonstr-arrow-up-thin-48-_1_.webp"
 
+let first_page_load = false
 const TrainingOverview = (props) => {
 
     const [limitOverView, setlimitOverView] = useState(true)
+
+    useEffect(() => {
+
+        if (limitOverView && first_page_load) {
+
+            setTimeout(() =>{
+
+                // Get the navigation bar element
+                const navbar = document.getElementById('navbar--sticky');zz
+    
+                // Get the element to scroll to
+                const syllabus = document.getElementById('course_syllabus');
+    
+                // Get the offset position of the element to scroll to
+                const syllabusOffset = syllabus.offsetTop;
+    
+                // Get the height of the navigation bar element
+                const navbarHeight = navbar.offsetHeight;
+    
+                // Calculate the final position to scroll to
+                const finalPosition = syllabusOffset - navbarHeight - 40;
+    
+                // Scroll to the final position
+                window.scrollTo({ top: finalPosition, behavior: 'smooth' });
+            },100)
+        } else {
+            first_page_load = true;
+        }
+    }, [limitOverView])
 
     return (
         <div id="course_overview">
@@ -21,17 +51,10 @@ const TrainingOverview = (props) => {
             <div className={`overview__wrapper relative ${limitOverView ? "limited" : ""}`} style={{
             }}>
                 <div dangerouslySetInnerHTML={{ __html: props?.course?.data?.course[0]?.description }} />
-                <div dangerouslySetInnerHTML={{ __html: props?.course?.data?.course[0]?.description }} />
-                <div dangerouslySetInnerHTML={{ __html: props?.course?.data?.course[0]?.description }} />
-                <div dangerouslySetInnerHTML={{ __html: props?.course?.data?.course[0]?.description }} />
-                <div dangerouslySetInnerHTML={{ __html: props?.course?.data?.course[0]?.description }} />
-                <div dangerouslySetInnerHTML={{ __html: props?.course?.data?.course[0]?.description }} />
-                <div dangerouslySetInnerHTML={{ __html: props?.course?.data?.course[0]?.description }} />
-                <div dangerouslySetInnerHTML={{ __html: props?.course?.data?.course[0]?.description }} />
                 <div className="fader absolute">
                 </div>
             </div>
-            <div className="m-8 text-center font-bold" style={{
+            <div className="m-8 text-center font-bold" id="overview__min_max" style={{
                 cursor: "pointer",
                 textTransform: "uppercase"
             }} onClick={() => { setlimitOverView(!limitOverView) }}>
