d03c23963af57cb2eb245d4212d0159facb00a76
Sagar <dev.sagartmg@gmail.com>
2023-03-29 12:32:29 +0545

    about us fix

--- components/AboutUsComponents/ApplyNow.tsx
+++ components/AboutUsComponents/ApplyNow.tsx
@@ -1,6 +1,6 @@
 const ApplyNow = () => {
   return (
-    <section className="w-full flex flex-col space-y-6 md:space-y-0 md:flex-row py-10  md:space-x-5 container mx-auto">
+    <section className="w-full flex flex-col space-y-6 md:space-y-0 md:flex-row py-10  md:space-x-5  mx-auto px-5 sm:px-20 lg:px-44">
       <h2 className='font-bold text-center md:text-left text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl w-full md:w-3/5' style={{ lineHeight: "1.3em",  }}>This is your chance to jumpstart your business</h2>
       <div className="w-full md:w-2/5 flex flex-col items-center md:items-start">
         <h2 className=' font-bold text-base md:text-xl  lg:text-2xl text-center md:text-left'>Interested in joining our team?</h2>
